93523dc211b6c828a41a54dde1f6943788a5e75a
EDA <eda@mac-mini-6.local>
2016-09-12 16:08:34 +1200

    massiff

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
 var development = require('./knexfile').development
 var knex = require('knex')(development)
 
-knex.insert([{
+// knex.insert([{
+//     name: 'mars bar',
+//     deliciousness: 'ultra yummy'
+//   }, {
+//     name: 'whittakers dark',
+//     deliciousness: 'gobsmackingly good'
+//   }])
+//   .into('candy')
+//   .then(getName)
+//   .then(showName)
+//   .catch(displayError)
+//
+// function getName(candy) {
+//   for (var i = 0; i < candy.length; i++) {
+//     console.log(candy[i].deliciousness)
+//   }
+//   return knex('candy').select('candy.deliciousness')
+// }
+//
+// function showName(name) {
+//   console.log(name)
+// }
+//
+// function displayError(err) {
+//   console.error(err)
+// }
 
-    name: 'mars bar',
-    deliciousness: 'ultra yummy'
-  }, {
-    name: 'whittakers dark',
-    deliciousness: 'gobsmackingly good'
+knex.update({
+  id: 2,
+  name: 'kit kat',
+  size: 'massive'
+})
 
-  }])
-  .into('candy')
+knex('candy')
+  .where('id',
+    '>', 4)
+  .update({
+    size: 'not so massiff'
+  })
   .then(getName)
   .then(showName)
-  .catch(displayError)
 
 function getName(candy) {
   for (var i = 0; i < candy.length; i++) {
-    console.log(candy[i].deliciousness)
+    console.log(candy[i].size)
   }
-  return knex('candy').select('candy.deliciousness')
+  return knex('candy').select('candy.size')
 }
 
-function showName(name) {
-  console.log(name)
-}
-
-function displayError(err) {
-  console.error(err)
+function showName(size) {
+  console.log(size)
 }
